22287c16ada70df8cd4da87db4895e274e3feddc
Mandzhi <bachaev@gmail.com>
2025-01-31 19:05:33 +0700

    January 31

---
+++ js/CE5.js
@@ -0,0 +1,14 @@
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    result = result + '*' + '\n';
+}
+
+
+// let m = 0;
+// while (m <= lines) {
+//     result = result + '*' + '\n';
+//     m++;
+// }
+console.log(result);
